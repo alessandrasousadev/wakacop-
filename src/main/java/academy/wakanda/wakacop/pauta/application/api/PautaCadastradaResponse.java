62698d74dd218a1b97066c1f711d6c1efd3ad9a6
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2024-04-20 23:33:08 -0300

    implementa flow repository pauta

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -1,6 +1,7 @@
 package academy.wakanda.wakacop.pauta.application.api;
 
 
+import academy.wakanda.wakacop.pauta.domain.Pauta;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -10,4 +11,7 @@ import java.util.UUID;
 public class PautaCadastradaResponse {
     private UUID id;
 
+    public PautaCadastradaResponse(Pauta pauta) {
+        this.id = pauta.getId();
+    }
 }
