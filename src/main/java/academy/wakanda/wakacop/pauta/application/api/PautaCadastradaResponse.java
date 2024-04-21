4d120dfc9c3a224856c948622afe3a1ab2c6594b
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2024-04-20 23:13:48 -0300

    adicionando @getter e @tostring em pautaCadastradaResponse

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -1,8 +1,12 @@
 package academy.wakanda.wakacop.pauta.application.api;
 
 
-import java.util.UUID;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.util.UUID;
+@Getter
+@ToString
 public class PautaCadastradaResponse {
     private UUID id;
 
